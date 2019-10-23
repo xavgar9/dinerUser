c6fda6048664f381414b0049b0d0de6740c77a90
Alejandro Meza <alejo5123@hotmail.com>
2019-10-23 18:21:49 -0500

    Update CRUD.sql

--- database/CRUD.sql
+++ database/CRUD.sql
@@ -169,6 +169,40 @@ END$$
 delimiter ;
 
 select validarConvenio(2);
+
+--------------------------------------------------------------------------------------
+/*Funcion que retorna booleano, si el DinerUser tiene membresia o no*/
+
+delimiter $$
+CREATE function userVIP(idDinerUser int)
+RETURNS BOOLEAN
+BEGIN
+declare consulta int;
+  select PK_idMembership into consulta from VIPMembership where FK_idDiner = idDinerUser;
+  if consulta is null then
+    RETURN False;
+  else
+    RETURN True;
+  end if;
+END$$
+delimiter ;
+
+--------------------------------------------------------------------------------------
+/*Funcion que retorna booleano, si el DinerUser tiene membresia o no*/
+
+delimiter $$
+CREATE function userVIP(idDinerUser int)
+RETURNS BOOLEAN
+BEGIN
+declare consulta int;
+  select PK_idMembership into consulta from VIPMembership where FK_idDiner = idDinerUser;
+  if consulta is null then
+    RETURN False;
+  else
+    RETURN True;
+  end if;
+END$$
+delimiter ;
 --*****************************************************************************************************************************************
 --PROCEDIMIENTOS:
 /*Procedimiento que crea un usuario en la tabla User*/
