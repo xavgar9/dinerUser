ce9630f3dedbdc7bb5c2d094d55ba4a36a80147c
monowilliam <aguirrewilliam-1996@hotmail.com>
2019-11-04 14:48:38 -0500

    update front

--- App.py
+++ App.py
@@ -549,6 +549,8 @@ def tinder():
 
         print(res_final)
         print(usr_final)
+        tinderlis = [["Mr. Wings", "Calle busquela # 25-48", "10/11/2019", "09:10 p.m.", "Carol", "Linda"],
+                     ["Alitas Factory", "Calle San Millan 25", "31/02/2020", "03:50 p.m.", "Sofia", "Guzman"]]
         lista1 = [["Mr. Wings","Carol","10/11/2019","09:30 p.m","Pendiente","misterwings","carol_alt"],
                   ["Martha","William","11/12/2019","10:30 p.m","Activo","americanpizzakw","williamaguirrezapata"],
                   ["3 x mil","Xavi","11/12/2019","10:30 p.m","Pendiente","hamburguerperfeito","pedromorrot"],
